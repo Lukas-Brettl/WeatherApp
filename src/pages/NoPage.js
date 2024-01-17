c59290826f1fad9e75ca477d3c615396ebb0ad44
Lukas <lukas.brettl@seznam.cz>
2024-01-17 20:31:08 +0100

    Upgrade NoPage

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,9 +1,13 @@
-import image from "../images/404-error-design-with-angry-clou.png"
+import image from "../images/404-error-image.jpg"
+import { Link } from "react-router-dom"
 const NoPage = () => {
   return (
-    <div style={{ background: "#0a243b", width: "100%", height: "100%" }} className="container-fluid d-flex justify-content-center align-items-center">
-      <img src={image} alt="404">
-      </img></div>
+    <div style={{position: "absolute", background: "#0a243b", width: "100%", height: "100%"}} 
+    className="container-fluid d-flex flex-column  justify-content-center align-items-center">
+      <img src={image} alt="404" id="chyba">
+      </img>
+      <Link to="/" style={{ textDecorationColor: "white"}}><h3 style={{ color: "white" }} className="pt-4">Přejít na hlavní stránku</h3></Link>
+    </div>
   )
 
 };
